3185317bbb2db6a058af619ba048a1067743204b
Kraememj <66258597+Kraememj@users.noreply.github.com>
2022-05-01 17:20:18 -0500

    Add files via upload

---
+++ GitNetSuite_RL.js
@@ -0,0 +1,26 @@
+define(['N/task'],(task) => {
+
+        /**
+         * @copyright 2022
+         * @author  Matt Kraemer
+         * @NApiVersion 2.1
+         * @NScriptType RESTlet
+         */
+        var exports = {};
+
+        const _post = context => {
+
+            let data = context
+            log.debug('data', data)
+            var mrTask = task.create({taskType: task.TaskType.MAP_REDUCE});
+            //Replace Script ID
+            mrTask.scriptId = 'customscript_add_files_to_file_cabinet';
+            //Replace Deployment Id
+            mrTask.deploymentId = 'customdeploy1';
+            //Create param in Map/Reduce Script Record
+            mrTask.params = {custscript_script_data: data};
+            mrTask.submit();
+        }
+        exports.post = _post;
+        return exports;
+    });
